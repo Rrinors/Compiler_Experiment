e4747cbf942b53ccd4ed8e01d16210586b4e236f
Rinors <13641064+rinors@user.noreply.gitee.com>
2023-12-06 20:36:27 +0800

    增加 NFA_to_DFA 的测试代码

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "FA.h"
 
 int main() {
-    std::string re = "a*b*(|c|d*)|((01)*23)*";
+    // std::string re = "a*b*(|c|d*)|((01)*23)*";
+    std::string re = "(a|b)*c";
     auto nfa = RE_to_NFA(re);
     nfa.show();
+    auto dfa = NFA_to_DFA(nfa);
+    dfa.show();
     return 0;
 }
