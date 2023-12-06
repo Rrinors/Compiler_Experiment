e9120f6e4b068546f5e7bf17895ca7106727acdd
Rinors <13641064+rinors@user.noreply.gitee.com>
2023-12-06 20:35:28 +0800

    增加start, accept, NFA_to_DFA

--- FA.h
+++ FA.h
@@ -4,13 +4,16 @@
 #include <vector>
 #include <string>
 
-class FA : std::vector<std::vector<std::pair<int, char>>> {
-public:
+struct FA : std::vector<std::vector<std::pair<int, char>>> {
+    int start;
+    std::vector<bool> accept;
     int new_node();
     void add_edge(int u, int v, char c = 0);
     void show();
 };
 
 FA RE_to_NFA(std::string s);
+
+FA NFA_to_DFA(FA nfa);
 
 #endif
