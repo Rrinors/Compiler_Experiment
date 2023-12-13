48f6abd25bf59cc6cedc70e4d6df54ca0270f1f5
Rinors <13641064+rinors@user.noreply.gitee.com>
2023-12-13 23:17:42 +0800

    增加提取左公因子测试代码

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include "Automaton.h"
 #include "Grammar.h"
+#include <iostream>
 
 int main() {
+    // 正则表达式转 NFA、NFA 转 DFA 和 DFA 最小化测试代码
     // Regular_Expression re = "a*b*(|c|d*)|((01)*23)*";
     // Regular_Expression re = "a(bc|dc)*";
     // auto nfa = RE_to_NFA(re);
@@ -9,16 +11,29 @@ int main() {
     // auto dfa = NFA_to_DFA(nfa);
     // dfa.show();
 
+    // 消除左递归测试代码
+    // Grammar g;
+    // g["S"].insert("Qc");
+    // g["S"].insert("c");
+    // g["Q"].insert("Rb");
+    // g["Q"].insert("b");
+    // g["R"].insert("Sa");
+    // g["R"].insert("a");
+    // show(g);
+    // std::cout << "====\n";
+    // g = rm_left_recursion(g);
+    // show(g);
+
+    // 提取左公因子测试代码
     Grammar g;
-    g["S"].insert("Qc");
-    g["S"].insert("c");
-    g["Q"].insert("Rb");
-    g["Q"].insert("b");
-    g["R"].insert("Sa");
-    g["R"].insert("a");
+    g["A"].insert("Bcd");
+    g["A"].insert("Bce");
+    g["A"].insert("Be");
+    g["B"].insert("acd");
+    g["B"].insert("bcd");
     show(g);
     std::cout << "====\n";
-    g = rm_left_recursion(g);
+    g = extract_LCF(g);
     show(g);
 
     return 0;
